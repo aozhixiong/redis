bdbb72afb33c580c7deabc78f832a228df5fe831
aozhixiong <651887301@qq.com>
2019-07-03 19:36:21 +0800

    Initial commit

---
+++ src/main/java/com.my/controller/Test.java
@@ -0,0 +1,19 @@
+import redis.clients.jedis.Jedis;
+
+public class Test {
+
+    public void fun1(){
+        //开启连接
+        Jedis jedis = new Jedis("localhost", 6379);
+        //切换库（默认0库）
+        jedis.select(0);
+        //存放数据
+        jedis.set("hello", "你好啊");
+        //获取数据
+        String string = jedis.get("hello");
+        //输出打印
+        System.out.println(string);
+        //回收资源
+        jedis.close();
+    }
+}
